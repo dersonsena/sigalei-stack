c3b81d4143bbc0da79bd45c7d49fa308de5bb3d6
Caio Flores <caiopflores@gmail.com>
2019-05-30 16:41:38 -0300

    chore: change permission method
    - remove user id validation.
    - return an error when not authorized.
    - add hasPermission in missing methods.

--- api/src/index.js
+++ api/src/index.js
@@ -9,14 +9,16 @@ schema.context = req => ({ ...req });
 schema.middlewares = middleware;
 const server = new GraphQLServer(schema);
 const options = {
-  port: process.env.GL_SERVER_PORT || 3000,
+  port: process.env.GL_SERVER_PORT || 5000,
   endpoint: process.env.GL_SERVER_ENDPOINT || '/graphql',
   playground: '/test',
-  context: req => ({ ...req }),
+  context: req => ({ ...req })
 };
 
 function serverStarted() {
-  console.log(`Server listening on port ${options.port} at endpoint ${options.endpoint}`);
+  console.log(
+    `Server listening on port ${options.port} at endpoint ${options.endpoint}`
+  );
   if (options.playground) {
     console.log(`Playground is active at endpoint ${options.playground}`);
   }

d4a53360e4922ea50a416d007d597da697d9acd0
Caio Flores <caiopflores@gmail.com>
2019-06-27 21:52:18 -0300

    chore: add new Model format proposal

--- api/src/graphql/task/Task.js
+++ api/src/graphql/task/Task.js
@@ -1,5 +1,15 @@
-module.exports = {
+import { resolver } from 'graphql-sequelize';
+import { Task } from '../../models';
+
+export default {
   Mutation: {
     createTask: async (obj, args, { auth: { user } }) => user.createTask(args)
+  },
+  Query: {
+    task: resolver(Task),
+    tasks: resolver(Task)
+  },
+  Task: {
+    user: resolver(Task.User)
   }
 };
